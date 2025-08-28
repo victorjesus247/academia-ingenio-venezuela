b888915f46ca6a7f383828df503ad217972a6885
victor lefebres <victor@correo.com>
2025-08-27 22:52:14 -0400

    intento 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -5,7 +6,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Añade esta línea para resolver el error de despliegue
   base: "/academia-ingenio-venezuela/", 
+  
   server: {
     host: "::",
     port: 8080,
